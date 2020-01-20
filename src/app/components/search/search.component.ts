72d1a03f72894d4136ad0a5f9c1f413527a022d4
Fraisin <appletree199@gmail.com>
2020-01-20 00:51:57 -0500

    Added image and title to front page.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "search",
-  templateUrl: "search.component.html"
+  templateUrl: "search.component.html",
+  styleUrls: ["./search.component.css"]
 })
 export class SearchComponent {
   title = "MusicDex";
