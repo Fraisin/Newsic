8619d14e46ecebc727ed55fbef842bcce5d88541
Fraisin <appletree199@gmail.com>
2020-01-27 00:40:57 -0500

    Capped genres at 6, displayed them in badges.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -46,10 +46,14 @@ export class SearchComponent {
     }
     return true;
   }
+  //method to take in an array of genres and cut it to length 6 if necessary
+  cutGenres(genres: any[]) {
+    if (genres.length <= 6) return genres;
+    return genres.slice(0, 6);
+  }
   //Method to dynamically change the donut status bar for an artist's popularity
   //Inspired from peavy's work @ https://codepen.io/peavy/pen/NzYVjw
-  updateDonutChart(el: any, percent: number) {
-    console.log(el);
+  updateDonutChart(chartID: any, percent: number) {
     percent = Math.round(percent);
     if (percent > 100) {
       percent = 100;
@@ -58,16 +62,16 @@ export class SearchComponent {
     }
     var deg = Math.round(360 * (percent / 100));
     if (percent > 50) {
-      $(el + " .pie").css("clip", "rect(auto, auto, auto, auto)");
-      $(el + " .right-side").css("transform", "rotate(180deg)");
+      $(chartID + " .pie").css("clip", "rect(auto, auto, auto, auto)");
+      $(chartID + " .right-side").css("transform", "rotate(180deg)");
     } else {
-      $(el + " .pie").css("clip", "rect(0, 1em, 1em, 0.5em)");
-      $(el + " .right-side").css("transform", "rotate(0deg)");
+      $(chartID + " .pie").css("clip", "rect(0, 1em, 1em, 0.5em)");
+      $(chartID + " .right-side").css("transform", "rotate(0deg)");
     }
-    $(el + " .right-side").css("border-width", "0.1em");
-    $(el + " .left-side").css("border-width", "0.1em");
-    $(el + " .shadow").css("border-width", "0.1em");
-    $(el + " .num").text(percent);
-    $(el + " .left-side").css("transform", "rotate(" + deg + "deg)");
+    $(chartID + " .right-side").css("border-width", "0.1em");
+    $(chartID + " .left-side").css("border-width", "0.1em");
+    $(chartID + " .shadow").css("border-width", "0.1em");
+    $(chartID + " .num").text(percent);
+    $(chartID + " .left-side").css("transform", "rotate(" + deg + "deg)");
   }
 }
